a86b5952c38aeb722f931d7e6b5ae37ce7f8b63c
romain sanson <romainsanson.gm@gmail.com>
2020-02-14 19:59:50 +0100

    add 404 page

--- routes/errors.js
+++ routes/errors.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
-    res.status(404).send('<h1>404</h1>')
+    res.status(404).sendFile(path.join(__dirname, '..', 'views', 'errors', '404.html'))
 })
 
 module.exports = router;
